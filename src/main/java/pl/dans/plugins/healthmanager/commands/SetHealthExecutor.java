cc976366d7bd89bcdee0c7d330c9b78d8a0a5c26
Graham <graham_howden1@yahoo.co.uk>
2014-05-23 22:12:35 +0100

    cleanup command

--- src/main/java/pl/dans/plugins/healthmanager/commands/SetHealthExecutor.java
+++ src/main/java/pl/dans/plugins/healthmanager/commands/SetHealthExecutor.java
@@ -1,5 +1,6 @@
 package pl.dans.plugins.healthmanager.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Server;
 import org.bukkit.command.Command;
@@ -10,47 +11,47 @@ import org.bukkit.entity.Player;
 public class SetHealthExecutor implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
-        if (command.getName().compareTo("setHealth") == 0) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!command.getName().equalsIgnoreCase("setHealth")) {
+            return false;
+        }
 
-            if (args.length != 2) {
-                return false;
-            }
+        if (args.length != 2) {
+            return false;
+        }
 
-            Server server = getServer();
-            String playerName = args[1];
-            Double health = Double.parseDouble(args[0]);
-            int percent = (int) ((health/20.0)*100);
-            if (playerName.compareTo("*") == 0) {
-                for (Player player : server.getOnlinePlayers()) {
-                    if (player.getMaxHealth() >= health) {
-                        player.setHealth(health);
-                    } else {
-                        sender.sendMessage(ChatColor.RED + "Couldn't set health for " + player.getName() + " - his maximum health is too low");
-                    }
-
-                }
-                getServer().broadcastMessage(ChatColor.GOLD + "Health of all players was set to " + percent + "%");
-            } else {
-                Player player = server.getPlayer(playerName);
-                if (player == null || !player.isOnline()) {
-                    sender.sendMessage(ChatColor.RED + "Player is not online.");
-                }
-
+        String playerName = args[1];
+        Double health = Double.parseDouble(args[0]);
+        int percent = (int) ((health/20.0)*100);
+        if (playerName.compareTo("*") == 0) {
+            for (Player player : Bukkit.getOnlinePlayers()) {
                 if (player.getMaxHealth() >= health) {
                     player.setHealth(health);
-                    player.sendMessage(ChatColor.GOLD + "Your health was set to " + percent + "%");
-                    if (player.getName().compareTo(sender.getName()) != 0) {
-                        sender.sendMessage(ChatColor.GOLD + playerName + "'s health was set to " + percent + "%");
-                    }
                 } else {
-                    sender.sendMessage(ChatColor.RED + "Couldn't set health for " + playerName + " - his maximum health is too low");
-                    return true;
+                    sender.sendMessage(ChatColor.RED + "Couldn't set health for " + player.getName() + " - his maximum health is too low");
                 }
-            }
-            return true;
 
+            }
+            Bukkit.broadcastMessage(ChatColor.GOLD + "Health of all players was set to " + percent + "%");
+            return true;
         }
-        return false;
+
+        Player player = Bukkit.getPlayer(playerName);
+        if (player == null || !player.isOnline()) {
+            sender.sendMessage(ChatColor.RED + "Player is not online.");
+        }
+
+        if (player.getMaxHealth() >= health) {
+            player.setHealth(health);
+            player.sendMessage(ChatColor.GOLD + "Your health was set to " + percent + "%");
+            if (player.getName().compareTo(sender.getName()) != 0) {
+                sender.sendMessage(ChatColor.GOLD + playerName + "'s health was set to " + percent + "%");
+            }
+        } else {
+            sender.sendMessage(ChatColor.RED + "Couldn't set health for " + playerName + " - his maximum health is too low");
+            return true;
+        }
+
+        return true;
     }
 }
