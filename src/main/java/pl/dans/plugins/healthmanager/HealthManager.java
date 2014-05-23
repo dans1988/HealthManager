e9d3a015f5471e76f8b615123e0d94077131e7e6
Graham <graham_howden1@yahoo.co.uk>
2014-05-23 21:50:48 +0100

    remove useless things

--- src/main/java/pl/dans/plugins/healthmanager/HealthManager.java
+++ src/main/java/pl/dans/plugins/healthmanager/HealthManager.java
@@ -1,39 +1,21 @@
 package pl.dans.plugins.healthmanager;
 
-import java.util.Set;
 import org.bukkit.ChatColor;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.Team;
 
-/**
- * Hello world!
- *
- */
-public class HealthManager extends JavaPlugin implements Listener
-{
-    public HealthManager() {
-        
-    }
+public class HealthManager extends JavaPlugin implements Listener {
     
     @Override
     public void onEnable() {
-        getLogger().info(new StringBuilder().append(ChatColor.RED).append("onEnable").toString());
         getServer().getPluginManager().registerEvents(this, this);
     }
-    
+
     @Override
-    public void onDisable() {
-        getLogger().info(new StringBuilder().append(ChatColor.RED).append("onDisable").toString());
-    }
-    
-     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
          if (command.getName().compareTo("setHealth") == 0) {
 
